9c5e0563959cd7614c754f5c60122b29c93caa96
LetsBee <hwe0613@gmail.com>
2021-03-30 23:13:58 +0900

    while 문을 통한 1~10 출력

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -10,7 +10,11 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    int number = 0;
+    do{
+        number++;
+        cout << number << endl;
+    }while(number < 10);
     return 0;
 }
